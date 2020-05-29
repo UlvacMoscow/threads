6abcac2f9dce8c5d6f3a18185718cefc4203e7ea
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2020-05-29 23:05:16 +0300

    add variable wict lock ping pong, no work how i wants

---
+++ part_5_1_lock_ping_pong.py
@@ -0,0 +1,31 @@
+import threading
+import time
+
+
+def ping():
+	while True:
+		time.sleep(0.2)
+		with lock:
+			print("ping =====================")
+
+
+def pong():
+	while True:
+		time.sleep(0.2)
+		with lock:
+			print("pong =============================================")
+
+
+# не работает должным образом при помощи блокировок
+
+lock = threading.RLock()
+# lock = threading.Lock()
+
+task1 = threading.Thread(target=ping)
+task2 = threading.Thread(target=pong)
+
+task1.start()
+task2.start()
+
+task1.join()
+task2.join()
